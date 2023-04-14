ba5f4629ed2909790adf1736c7db9e070e7a9c1d
RohitBeniwal <beniwalrb177@gmail.com>
2023-04-15 01:33:09 +0530

    commit 1 home page

---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,14 @@
+import {Canvas} from '@react-three/fiber'
+import {Environment,Center} from '@react-three/drei'
+
+import Shirt from './Shirt';
+import Backdrop from './BackDrop';
+import CameraRig from './CameraRig';
+
+const CanvasModel = () => {
+  return (
+    <div>Canvas</div>
+  )
+}
+
+export default CanvasModel
